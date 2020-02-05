67820dd143af09a6bb64e391e5f54230a4e325f6
Cedric Robitaille <cedric@Cedrics-Glorified-Toaster.local>
2020-02-05 00:12:04 -0500

    Create base banner and card content

---
+++ _patterns/cards/cards.js
@@ -0,0 +1,15 @@
+    var colors = ['#C38376', '#D1CBAA', '#95CAD9', '#A9A1E8', '#B7E2B3', '#F8AC9C', '#9ECB86', '#C7AD8A', '#D88A93', '#8DD1AC', '#75DDC1'];
+    var cards=document.querySelectorAll(".card-color");
+
+[].forEach.call(cards, (elem)=>{
+  var random_color = colors[Math.floor(Math.random() * colors.length)];
+  var btn = elem.querySelector(".btn");
+  var card = elem.querySelector(".card-color");
+
+  if (btn) {
+    btn.style.color = random_color;
+  }
+
+  elem.style.backgroundColor = random_color;
+
+})
