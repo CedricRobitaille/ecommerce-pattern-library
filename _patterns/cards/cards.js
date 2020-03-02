2bed5e2a5819231c99a504c5bb7317cd00c76524
Cedric Robitaille <cedric@Cedrics-Glorified-Toaster.local>
2020-03-02 15:10:36 -0500

    Create form items

--- _patterns/cards/cards.js
+++ _patterns/cards/cards.js
@@ -5,13 +5,22 @@
   var random_color = colors[Math.floor(Math.random() * colors.length)];
   var btn = elem.querySelector(".btn");
   var card = elem.querySelector(".card-color");
-  var input = elem.querySelector(".input");
+  var inputs = elem.querySelectorAll(".input");
+  var button = elem.querySelector(".buttons")
+
 
   if (btn) {
     btn.style.color = random_color;
   }
-  if (input) {
-    input.style.color = random_color;
+
+  if (button) {
+    button.style.color = random_color;
+  }
+
+  if (inputs) {
+    [].forEach.call(inputs, (inp)=>{
+      inp.style.color = random_color;
+    });
   }
 
   elem.style.backgroundColor = random_color;
